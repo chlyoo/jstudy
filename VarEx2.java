0c1b22ab485d71bbe1c29771c3fe21d87738efc3
chlyoo <changhyunlyoo@gmail.com>
2020-11-02 00:34:42 +0900

    variable example

---
+++ VarEx2.java
@@ -0,0 +1,15 @@
+class VarEx2{
+	public static void main(String[] args){
+	int x = 10, y=20;
+	int temp = 0;
+
+	System.out.println("x: "+ x + " y:" + y);
+
+	temp = x;
+	x = y;
+	y = temp;
+
+	System.out.println("x: "+ x + " y:" + y);
+
+	}
+}
