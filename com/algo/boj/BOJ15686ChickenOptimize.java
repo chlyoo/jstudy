b1f43fcb3c1e547445af951dc03bcbd5d70a5c28
chlyoo <changhyunlyoo@gmail.com>
2021-02-21 22:33:31 +0900

    import optimize

--- com/algo/boj/BOJ15686ChickenOptimize.java
+++ com/algo/boj/BOJ15686ChickenOptimize.java
@@ -3,9 +3,6 @@ package com.algo.boj;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Iterator;
 import java.util.List;
 import java.util.StringTokenizer;
 
