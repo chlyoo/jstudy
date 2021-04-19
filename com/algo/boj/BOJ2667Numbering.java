6e6e4c48e06de0b7d3943213a980c221cf3f6193
= <changhyunlyoo@gmail.com>
2021-04-19 09:09:55 +0900

    optimized

--- com/algo/boj/BOJ2667Numbering.java
+++ com/algo/boj/BOJ2667Numbering.java
@@ -5,7 +5,6 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
-import java.util.Queue;
 
 public class BOJ2667Numbering {
 	static class Node{
@@ -16,12 +15,10 @@ public class BOJ2667Numbering {
 			this.x = x;
 			this.y = y;
 		}
-
 	}
 
 	private static int[][] map;
 	private static int N, mini , minj;
-	private static boolean[][] visited;
 	private static int[] dx= {1,-1,0,0};
 	private static int[] dy= {0,0,1,-1};
 	private static ArrayList<Integer> rank;
@@ -37,7 +34,6 @@ public class BOJ2667Numbering {
 				map[i][j] = line.charAt(j)-'0';
 			}
 		}
-		visited = new boolean[N][N];
 		rank = new ArrayList<>();
 		bfs();
 		sb.append(rank.size()).append("\n");
@@ -50,12 +46,11 @@ public class BOJ2667Numbering {
 	}
 
 	private static void bfs() {
-		Queue<Node> q = new LinkedList<>();
+		LinkedList<Node> q = new LinkedList<>();
 		Node s = selectOne(mini, minj);
 		int cnt = 0;
 		while(s!=null) {
 			q.offer(s);
-			visited[s.x][s.y]=true;
 			map[s.x][s.y]=0;
 			cnt= 1;
 			while (!q.isEmpty()) {
@@ -65,13 +60,10 @@ public class BOJ2667Numbering {
 					for (int d = 0; d < 4; d++) {
 						int nx =n.x+dx[d];
 						int ny =n.y+dy[d];
-						if(nx<0 || ny<0 || nx>=N || ny>=N) continue;
-						if(visited[nx][ny]) continue;
-						if(map[nx][ny]==0)continue;
-						map[nx][ny]=0;
-						visited[nx][ny]=true;
-						q.offer(new Node(nx,ny));
+						if(nx<0 || ny<0 || nx>=N || ny>=N || map[nx][ny]==0) continue;
 						cnt++;
+						map[nx][ny]=0;
+						q.offer(new Node(nx,ny));
 					}
 				}
 			}
@@ -88,7 +80,6 @@ public class BOJ2667Numbering {
 			}else
 				c=0;
 			for (int j = c; j < N; j++) {
-
 				if(map[i][j]==1) {
 					mini = i;
 					minj = j;
